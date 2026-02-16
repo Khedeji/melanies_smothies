1fc2cae5983e034687b49516cc87e936cf3d4976
Khedeji <138321719+Khedeji@users.noreply.github.com>
2026-02-16 11:43:38 +0530

    Include SEARCH_ON in fruit options and display
    Added SEARCH_ON column to fruit options and displayed it in the dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,9 @@ st.write("The name on Your Smoothie will be", title)
 cnx = st.connection("snowflake")
 
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect('Choose upto 5 ingredients:',
@@ -31,6 +33,8 @@ if ingredients_list:
     for fruit in ingredients_list :
         ingredients_string += fruit + ' '
         st.subheader(fruit + ' Nutrition Information')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit)
         # st.text(smoothiefroot_response.json())
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
